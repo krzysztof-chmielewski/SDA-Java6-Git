68a885543fa857c096a93084669219ceda81d030
Krzysztof Chmielewski <chmielewski.k.s@gmail.com>
2018-01-23 10:57:00 +0100

    Fixed division by 0

--- src/main/java/com/kchmielewski/sda/java6/git/Calculator.java
+++ src/main/java/com/kchmielewski/sda/java6/git/Calculator.java
@@ -14,6 +14,9 @@ public class Calculator {
     }
 
     public float by(float a, float b) {
+        if (b == 0f) {
+            throw new IllegalArgumentException("Division by 0!");
+        }
         return a / b;
     }
 }
